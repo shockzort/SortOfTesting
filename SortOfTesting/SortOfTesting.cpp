19bc310a55d42242c105faf363d926b1f083292a
Shockzor <shockus@gmail.com>
2021-05-14 17:47:29 +0300

    added insertion sort, and support for containers

--- SortOfTesting/SortOfTesting.cpp
+++ SortOfTesting/SortOfTesting.cpp
@@ -5,19 +5,20 @@
 #include <memory>
 #include <vector>
 #include "RandomSampler.h"
+#include <exception>
 
-template<typename Iterator>
-void sort_by_selection(Iterator begin, Iterator end)
+template<typename container_type, typename T = typename container_type::value_type>
+void sort_by_selection(container_type& container)
 {
-	if (end - begin <= 1)
+	if (container.size() <= 1)
 		return;
 
-	for (auto it = begin; it != end - 1; ++it) 
+	for (auto it = container.begin(); it != container.end() - 1; ++it)
 	{
 		auto cur_min = it;
 		auto cur_min_val = *it;
 
-		for (auto it_inner = it + 1; it_inner != end; ++it_inner)
+		for (auto it_inner = it + 1; it_inner != container.end(); ++it_inner)
 		{
 			if (*it_inner < cur_min_val)
 			{
@@ -33,32 +34,62 @@ void sort_by_selection(Iterator begin, Iterator end)
 	}
 }
 
-int main()
+
+template<typename container_type, typename T = typename container_type::value_type>
+void sort_by_insertion(container_type& container)
 {
-	std::cout << "Hello. This is the sorting algorithm testing software\n";
-	std::cout << "We are going to test and compare two algorithms : Selection and Insertion sorting\n";
+	if (container.size() <= 1)
+		return;
 
-	std::cout << "Generating random data for Selection sorting algorithm\n";
+	for (auto idx_out = 1; idx_out < container.size(); ++idx_out)
+	{
+		auto cur_min_val = container[idx_out];
+		
+		auto idx_inner = idx_out - 1;
+		for (; idx_inner >= 0 && container[idx_inner] > cur_min_val; --idx_inner)
+		{
+			container[idx_inner + 1] = container[idx_inner];
+		}
 
+		container[idx_inner + 1] = cur_min_val;
+	}
+}
+
+void testSorting_algorithms()
+{
+	std::cout << "We are going to test and compare two algorithms : Selection and Insertion sorting" << std::endl;
+	
 	RandomSampler sampler;
 
 	static const std::vector<int> test_data_lengths = { 2, 16, 32, 128, 1024, 8192 };
 
+	std::cout << "Generating random data for Selection sorting algorithm" << std::endl;
 	auto test_array_set_1 = sampler.generateRandomArrays<float>(test_data_lengths);
 
+	std::cout << "Generating random data for Insertion sorting algorithm" << std::endl;
 	auto test_array_set_2 = sampler.generateRandomArrays<int>(test_data_lengths);
-	
 
-	for (auto & array : test_array_set_1) 
+
+	std::cout << "Sorting by Selection sorting algorithm" << std::endl;
+	for (auto& array : test_array_set_1)
 	{
-		sort_by_selection(array.begin(), array.end());
+		sort_by_selection(array);
 	}
 
-
+	std::cout << "Sorting by Insertion sorting algorithm" << std::endl;
 	for (auto& array : test_array_set_2)
 	{
-		sort_by_selection(array.begin(), array.end());
+		sort_by_insertion(array);
 	}
+}
+
+int main()
+{
+	std::cout << "Hello. This is the sorting algorithm testing software" << std::endl;
+	
+	testSorting_algorithms();
+	
+	std::cout << "Done testing. Goodbue!" << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
